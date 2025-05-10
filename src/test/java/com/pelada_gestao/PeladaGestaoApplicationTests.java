ad412b7438a97a95b3ff79a9b02a6c0a6cf8d93f
Wallaks Cardoso da Silva <wallaks.silva@matera.com>
2025-05-10 14:01:45 -0300

    Projeto inicial gerado com Spring Initializr

---
+++ src/test/java/com/pelada_gestao/PeladaGestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pelada_gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeladaGestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
